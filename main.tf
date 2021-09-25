522152bc6e680f4cc910663380eeb7d74d3ab436
learncode2100 <88165603+learncode2100@users.noreply.github.com>
2021-09-25 15:17:23 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,17 +8,40 @@ terraform {
         version = "> 2.71.0"
     }
   }
+  backend "remote" {
+    organization = "learncode2100-demo"
+    workspaces {
+    name = "Example-Workspace"
+    }
+  }
 }
 
 provider "azurerm" {
   features {
   }
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  tenant_id       = var.ARM_TENANT_ID
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+  
+}
+variable "ARM_CLIENT_ID" {
+  
+}
+variable "ARM_CLIENT_SECRET" {
+  
+}
+variable "ARM_TENANT_ID" {
+  
 }
 
 resource "azurerm_resource_group" "testrg" {
-  name = "testrg002"
+  name = "testrg003"
   location = "westeurope"
   tags = {
-    "env" = "dev"
+    "env" = "prod"
   }
 }
